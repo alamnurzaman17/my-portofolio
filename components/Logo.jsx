da4e1c8c26a52a20479ba8d73b913596e08716ac
Alam Nurzaman <alam.nurzaman17@gmail.com>
2024-04-14 20:15:25 +0700

    Pembaruan logo pada nav

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,11 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import ThemeToggler from "./ThemeToggler";
+import { useTheme } from "next-themes";
 
 const Logo = () => {
+  const { theme } = useTheme(ThemeToggler);
+
   return (
     <Link href="/">
       <Image
-        src="/log-logo.svg"
+        src={theme === "dark" ? "/log-logowhite.svg" : "/log-logo.svg"}
         alt="logo"
         width={54}
         height={54}
